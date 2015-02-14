9590aa25ddf88bbd0af0d8b30d9625af9c41d41b
LI.Reaver() <desiconsa@gmail.com>
2015-02-14 09:53:09 +0300

    Create shellmasm611.asm
    First to be made
    ;SIMPLESHELL

---
+++ shellmasm611.asm
@@ -0,0 +1,19 @@
+cseg segment
+assume cs:cseg, ds:cseg, es:cseg, ss:cseg
+org 100h
+
+begin_shell:
+call check_video
+mov ah,9
+mov dx,offset mes
+int 21h
+call amin_proc
+int 20h
+include main.asm
+include display.asm
+include files.asm
+include keyboard.asm
+include messages.asm
+;more asms go here
+cseg ends
+begin_shell endp
